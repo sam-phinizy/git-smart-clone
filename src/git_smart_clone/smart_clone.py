17dc04f2de91af93a9bed969eb965ca1bead80d8
Sam Phinizy <sam@phin.tech>
2022-08-22 08:15:09 -0400

    add in support for sourcehut urls

--- src/git_smart_clone/smart_clone.py
+++ src/git_smart_clone/smart_clone.py
@@ -7,18 +7,36 @@ import subprocess
 
 import giturlparse
 
+
+def parse_git_url(url: str) -> giturlparse.parser.Parsed:
+    parsed_url = giturlparse.parse(url)
+
+    if 'sr.ht' in url:
+        return giturlparse.parser.Parsed(
+            parsed_url.pathname,
+            parsed_url.protocols,
+            parsed_url.protocol,
+            parsed_url.href,
+            'sr.ht',
+            parsed_url.user,
+            parsed_url.port,
+            parsed_url.name,
+            parsed_url.resource
+        )
+
+    return parsed_url
+
 @click.command(context_settings=dict(
     ignore_unknown_options=True,
 ))
 @click.argument("repo",type=str)
 @click.argument("args", nargs=-1, type=click.UNPROCESSED)
 def git_smart_clone(repo:str,args):
-    print(args)
     home_folder = pathlib.Path.home()
     base_path = os.environ.get("GIT_SMART_CLONE_BASE_PATH",
                                home_folder / 'src')
 
-    url = giturlparse.parse(repo)
+    url = parse_git_url(repo)
     destination_path = base_path / url.resource / url.owner / url.name
 
     # make the path
